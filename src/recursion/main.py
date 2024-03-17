ded81d2d9bcb032fba3afaf36aaf31ad58a893a2
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-03-16 19:49:28 -0500

    Recursion worksheet done

--- src/recursion/main.py
+++ src/recursion/main.py
@@ -7,9 +7,8 @@ import backtracking.chess_horse as chess_horse
 import src.root3.root3 as root3
 import backtracking.skippers as skippers
 
-
-#1.
-print("Taller 1 - Diagnóstico\n")
+# 0.
+print("DIAGNÓSTICO\n")
 
 sumar_enteros(1, 2)
 
@@ -21,8 +20,8 @@ imprimir_impares(10)
 
 print(f'{sumar_elementos(list(range(1, 11)))}\n')
 
-#2.
-print("Taller 2 - Recursión\n")
+# 1.
+print("RECURSIÓN\n")
 
 print(suma([1, 2, 3, 4, 5]))
 print(ocurrencias(14345, 4))
@@ -33,18 +32,25 @@ print(f'1627384950 tiene {cant_pares(1627384950)} pares')
 print(f'1230 tiene {cant_pares(1230)} pares')
 print(f'108509 tiene {cant_pares(108509)} pares')
 print(menor([5, 2, 7, 4, 3, 1]))
+print(mcd(36, 78))
+print(suma([1, 2, 3, 4, 5, 4, 1, 3]))
+print(cuadrado(12))
+print(fibonacci(12))
+print(binario(127))
+print(invertir("desmadre"))
+print(primera_ocurrencia([1, 2, 3, 4, 5, 4, 1, 3], 3))
+print(ultima_ocurrencia([1, 2, 3, 4, 5, 4, 1, 3], 4))
+print(mas_frecuente_letra(
+	["a", "e", "i", "o", "u"],
+	"esternocleidomastoideo"
+))
 
 # Classwork - Intro to backtracking
 print("Trabajo de clase - Backtracking\n")
 maze.backtrack(maze.maze)
 print()
 chess_horse.backtrack(chess_horse.table)
+print()
 
-# 3.
-print("Taller 3 - Backtracking\n")
-
-# Test
-#root3.exec()
-skippers.backtrack(copy.deepcopy(skippers.formation))
-
-# Survival won
+# 2.
+print("BACKTRACKING\n")
--- src/recursion/recursion/recursion.py
+++ src/recursion/recursion/recursion.py
@@ -1,77 +1,168 @@
+import copy
+
+
 def cant_digitos(n: int) -> int:
-    '''
-    Cuenta la cantidad de dígitos en un número natural N.
-    '''
-    if n < 10:
-        return 1
-    else:
-        return cant_digitos(n // 10) + 1
+	'''
+	Cuenta la cantidad de dígitos en un número natural N.
+	'''
+	if n < 10:
+		return 1
+	else:
+		return 1 + cant_digitos(n // 10)
 
 def ocurrencias(n: int, d: int) -> int:
-    '''
-    Cuenta las ocurrencias de un dígito D en un número natural N.
-    '''
-    if n < 10:
-        return 1 if n == d else 0
-    elif n % 10 == d:
-        return ocurrencias(n // 10, d) + 1
-    else:
-        return ocurrencias(n // 10, d)
+	'''
+	Cuenta las ocurrencias de un dígito D en un número natural N.
+	'''
+	if n < 10:
+		return 1 if n == d else 0
+	elif n % 10 == d:
+		return 1 + ocurrencias(n // 10, d)
+	else:
+		return ocurrencias(n // 10, d)
 
 def invertir(n: int, m: str = "") -> int:
-    '''
-    Invierte los dígitos de un número natural N
-    '''
-    if n < 10:
-        return n
-    else:
-        return (n % 10) * (10 ** (cant_digitos(n) - 1)) + invertir(n // 10)
-    """
-        return f"{m}{n}"
-    else:
-        return invertir(n // 10, f"{m}{n % 10}")
-    """
-    
+	'''
+	Invierte los dígitos de un número natural N
+	'''
+	if n < 10:
+		return n
+	else:
+		return (n % 10) * (10 ** (cant_digitos(n) - 1)) + invertir(n // 10)
+	"""
+		return f"{m}{n}"
+	else:
+		return invertir(n // 10, f"{m}{n % 10}")
+	"""
+	
+	
 def cant_pares(n: int) -> int:
-    '''
-    Cuenta las ocurrencias de dígitos pares en un número natural N
-    '''
-    if n < 10:
-        return 1 if n % 2 == 0 else 0
-    else:
-        if n % 2 == 0:
-            return cant_pares(n // 10) + 1
-        else:
-            return cant_pares(n // 10)
+	'''
+	Cuenta las ocurrencias de dígitos pares en un número natural N
+	'''
+	if n < 10:
+		return 1 if n % 2 == 0 else 0
+	else:
+		if n % 2 == 0:
+			return cant_pares(n // 10) + 1
+		else:
+			return cant_pares(n // 10)
 
 def menor(lista: list) -> int:
-    '''
-    Encuentra el elemento menor dentro de una lista
-    '''
-    if len(lista) == 1:
-        return lista[0]
-    else:
-        if lista[1] < lista[0]:
-            del lista[0]
-        else:
-            del lista[1]
-        return menor(lista)
-    
+	'''
+	Encuentra el elemento menor dentro de una lista
+	'''
+	if len(lista) == 1:
+		return lista[0]
+	else:
+		if lista[1] < lista[0]:
+			del lista[0]
+		else:
+			del lista[1]
+		return menor(lista)
+	
 def mcd(a: int, b: int) -> int:
-    '''
-    Algoritmo de Euclides para calcular el MCD de dos números.
-    '''
-    if b == 0:
-        return a
-    else:
-        return mcd(b, a % b)
-    
+	'''
+	Algoritmo de Euclides para calcular el MCD de dos números.
+	'''
+	if b == 0:
+		return a
+	else:
+		return mcd(b, a % b)
+	
 def suma(vector: list, res: int = 0) -> int:
-    '''
-    Suma los elementos de un vector de manera recursiva
-    '''
-    if len(vector) == 1:
-        return res + vector[0]
-    else:
-        value = vector.pop()
-        return suma(vector, res + value)
+	'''
+	Suma los elementos de un vector de manera recursiva
+	'''
+	if len(vector) == 1:
+		return res + vector[0]
+	else:
+		value = vector.pop()
+		return suma(vector, res + value)
+
+def cuadrado(n: int):
+	'''
+	Calcula el cuadrado de un número N entero positivo de manera recursiva,
+	como la sumatoria de los primeros N números impares.
+	'''
+	if n < 1:
+		return 0
+	else:
+		return (2 * n - 1) + cuadrado(n - 1)
+
+def fibonacci(n: int):
+	'''
+	Calcula el n-ésimo término de la sucesión de Fibonacci.
+	'''
+	if n > 0:
+		if n in [1, 2]:
+			return 1
+		else:
+			return fibonacci(n - 1) + fibonacci(n - 2)
+	else:
+		return 0
+
+def binario(n: int):
+	'''
+	Retorna la representación binaria de un número decimal como entero.
+	'''
+	if n < 2:
+		return n
+	else:
+		return binario(n // 2) * 10 + (n % 2)
+
+def invertir(s: str):
+	'''
+	Invierte una cadena de texto recursivamente.
+	'''
+	if len(s) == 1:
+		return s
+	else:
+		return s[-1] + invertir(s[:-1])
+
+def primera_ocurrencia(vector: list, n: int):
+	'''
+	Encuentra la primera posición en la que aparece un valor dado en una lista de enteros.
+	'''
+	if vector[0] == n:
+		return 0
+	else:
+		return primera_ocurrencia(vector[1:], n) + 1
+
+def ultima_ocurrencia(vector: list, n: int):
+	'''
+	Encuentra la última posición en la que aparece un valor dado en una lista de enteros.
+	'''
+	if vector[-1] == n:
+		return len(vector) - 1
+	else:
+		return ultima_ocurrencia(vector[:-1], n)
+	
+def _mas_frecuente_letra(letras: list[str], s: str, sln: list = []):
+	if len(s) == 0:
+		return letras[sln.index(
+			mayor(copy.deepcopy(sln))
+		)]
+	else:
+		if s[0] in letras:
+			sln[letras.index(s[0])] += 1
+		return _mas_frecuente_letra(letras, s[1:], sln)
+
+def mas_frecuente_letra(letras: list[str], s: str):
+	'''
+	Encuentra la letra con mayor número de instancias en una cadena de texto.
+	'''
+	return _mas_frecuente_letra(letras, s, len(letras) * [0])
+
+def mayor(lista: list) -> int:
+	'''
+	Encuentra el elemento mayor dentro de una lista
+	'''
+	if len(lista) == 1:
+		return lista[0]
+	else:
+		if lista[1] > lista[0]:
+			del lista[0]
+		else:
+			del lista[1]
+		return mayor(lista)
