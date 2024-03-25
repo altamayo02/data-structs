4d0dc8bc4dcc8d3aa1806f826d7d04c695ae709a
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-03-25 16:35:03 -0500

    N Queens fundamental solutions calculator implemented

--- src/other/queens/n_queens.py
+++ src/other/queens/n_queens.py
@@ -16,6 +16,7 @@ class Board:
 		string = ""
 		for i in range(len(self.matrix)):
 			string += f'{" ".join(self.matrix[i])}\n'
+		string += "-----------------------------"
 		return string
 
 def rotate(board):
@@ -41,7 +42,7 @@ def mirror(board):
 
 # Thanks cmc!
 # https://leetcode.com/problems/n-queens/solutions/19810/fast-short-and-easy-to-understand-python-solution-11-lines-76ms
-def solve_n_queens(n):
+def solve_board(n):
 	def DFS(queens, xy_dif, xy_sum):
 		p = len(queens)
 		if p==n:
@@ -54,9 +55,34 @@ def solve_n_queens(n):
 	DFS([],[],[])
 	return [ [". "*i + "Q" + " ."*(n-i-1) for i in sol] for sol in result]
 
-def read_n_queens(path: str):
+def write_solutions(n, path: str):
+	with open(f"{path}/{n}_queens.md", 'w') as file:
+		boards = solve_board(n)
+		for b in range(len(boards)):
+			for row in boards[b]:
+				file.write(f"{row}\n")
+			if b + 1 < len(boards):
+				file.write("-----------------------------\n")
+			else:
+				file.write("-----------------------------")
+
+def write_fundamentals(n, path: str):
+	with open(f"{path}/{n}_fundamentals.md", 'w') as file:
+		boards = read_solutions(n, path)
+		funds = fundamentals(boards)
+		for b in range(len(funds)):
+			for row in funds[b].matrix:
+				for col in row:
+					file.write(f"{col} ")
+				file.write("\n")
+			if b + 1 < len(funds):
+				file.write("-----------------------------\n")
+			else:
+				file.write("-----------------------------")
+
+def read_solutions(n, path: str):
 	list_boards: list[Board] = []
-	with open(path, 'r') as file:
+	with open(f"{path}/{n}_queens.md", 'r') as file:
 		string = file.read()
 		str_boards = string.split("-----------------------------\n")
 		for b in range(len(str_boards)):
@@ -71,24 +97,51 @@ def read_n_queens(path: str):
 					list_boards[b].matrix[r].append(c)
 	return list_boards
 
-def fundamentals(boards):
-	boards: list[Board] = boards
+def fundamentals(og_boards):
+	boards: list[Board] = copy.deepcopy(og_boards)
 
-	fundamentals: list[Board] = []
-	percentage = 0
-	for b1 in boards:
-		print(f"{100 * percentage / len(boards):.2f}%")
-		if b1 not in fundamentals:
-			fundamentals.append(b1)
-		for b2 in boards:
-			same = False
-			for _ in range(4):
-				same = b2 == b1 or mirror(b2) == b1
-				b2 = rotate(b2)
-			if not same:
-				fundamentals.append(b2)
-		percentage += 1
+	b = 0
+	while b < len(boards):
+		# Mirror of the current board
+		for b2 in range(len(boards)):
+			# Verbose
+			perc = percentage(len(boards), b, 0.25 * b2)
+			print(f"{perc:.10f}%")
+			if b == b2: continue
+			if mirror(boards[b2]) == boards[b]:
+				del boards[b2]
+				if b >= len(boards): return boards
+				break
+		temp_board = boards[b]
+		for i in range(3):
+			temp_board = rotate(temp_board)
+			for b2 in range(len(boards)):
+				perc = percentage(len(boards), b, (i + 2) * 0.25 * b2)
+				print(f"{perc:.10f}%")
+				if b == b2: continue
+				# Rotation of the current board
+				if boards[b2] == temp_board:
+					# print(boards[b2] == temp_board)
+					# print(mirror(boards[b2]) == temp_board)
+					del boards[b2]
+					if b >= len(boards): return boards
+					break
+			for b2 in range(len(boards)):
+				if b == b2: continue
+				# Mirror of the rotation of the current board
+				if mirror(boards[b2]) == temp_board:
+					del boards[b2]
+					if b >= len(boards): return boards
+					break
+		b += 1
+	return boards
 
-boards = read_n_queens('./src/other/queens/n_queens.md')
-# TODO - Prints NONE, should print a list of 46 items
-print(fundamentals(boards))
+def percentage(board_size, b, b2):
+	return (100 * b + 10 * b2) / board_size
+
+def solve(n):
+	write_solutions(n, './src/other/queens/md')
+	write_fundamentals(n, './src/other/queens/md')
+	print(f"Check md folder for {n}_queens.md and {n}_fundamentals.md")
+
+solve(12)
